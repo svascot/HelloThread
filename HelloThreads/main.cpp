da7c19a60bbd9aaf249e2c6673490c74959770c2
Santiago Vasco Tabares <santiago.vasco@coit004995.local>
2019-01-19 16:38:00 -0500

    Initial Commit

---
+++ HelloThreads/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloThreads
+//
+//  Created by Santiago Vasco Tabares on 1/19/19.
+//  Copyright © 2019 Santiago Vasco Tabares. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
